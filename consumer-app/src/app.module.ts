a1d1ec4adb371ff3367ba488bcb438b2b6a44ab2
ilya <misyurailya97@gmail.com>
2024-10-26 21:04:38 +0300

    handle consumer

--- consumer-app/src/app.module.ts
+++ consumer-app/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Kafka, logLevel } from 'kafkajs';
 
 @Module({
-  imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: 'KAFKA_CLIENT',
+      useFactory: () => {
+        return new Kafka({
+          brokers: (process.env.KAFKA_BROKERS || '').split(','),
+          ssl: false,
+          sasl: {
+            mechanism: 'plain',
+            username: process.env.KAFKA_SASL_USERNAME as string,
+            password: process.env.KAFKA_SASL_PASSWORD as string,
+          },
+          logLevel: logLevel.INFO,
+        });
+      },
+    },
+    AppService,
+  ],
+  exports: ['KAFKA_CLIENT'],
 })
 export class AppModule {}
--- consumer-app/src/app.service.ts
+++ consumer-app/src/app.service.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Consumer, Kafka } from 'kafkajs';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnModuleInit {
+  private readonly logger = new Logger(AppService.name);
+  private consumer: Consumer;
+
+  constructor(@Inject('KAFKA_CLIENT') private readonly kafka: Kafka) {}
+
+  async onModuleInit() {
+    await this.initKafkaConsumer();
+  }
+
+  private async initKafkaConsumer() {
+    this.consumer = this.kafka.consumer({ groupId: 'nestjs-consumer-group' });
+    await this.consumer.connect();
+    await this.consumer.subscribe({ topic: 'test-topic', fromBeginning: true });
+
+    await this.consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        this.logger.log({
+          value: message.value.toString(),
+          topic,
+          partition,
+          key: message.key?.toString(),
+        });
+      },
+    });
   }
 }
